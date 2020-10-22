2f0e1d1a3f46716ed6b0cf01e38c6762e7097bf5
Austin Lambert <atlambert11@gmail.com>
2020-10-22 18:55:16 -0500

    Ungh

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const fs = require("fs");
 
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 //Middleware
 app.use(express.urlencoded({ extended: true }));
